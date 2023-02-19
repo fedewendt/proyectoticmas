da915fc135cb471479416bdaa5a08aa406279ce9
fedewendt <80305360+fedewendt@users.noreply.github.com>
2023-02-18 23:25:06 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,24 @@
+//Aqui se ejecuta la funcion para enviar el formulario de contacto
+
+function enviar(){
+    const envio = enviar();
+    document.getElementById('enviar').innerHTML = envio;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
